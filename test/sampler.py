5b440a2486f614fe82f9f38bd3994cd9f22f8339
Hiruma31 <romain.grenet@gmail.com>
2014-04-16 16:22:53 +0900

    Tests

---
+++ test/sampler.py
@@ -0,0 +1,36 @@
+__author__ = 'Hiruma'
+
+LANG = "fr"
+import os
+import urllib
+import urllib2
+
+def sample():
+    """ Function to sample some pre-recorded answers."""
+    text_to_sample = raw_input("Text to sample:")
+    # TODO Check limit 100 characters
+    url = "http://translate.google.com/translate_tts?tl="+LANG+"&q="
+    #values = urllib.urlencode({"q": text, "textlen": len(text), "tl": lang})
+    values = urllib.urlencode({"q": text_to_sample})
+    hdrs = {"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7"}
+    req = urllib2.Request(url, data=values, header=hdrs)
+    p = urllib2.urlopen(req)
+    # Save
+    file_name = raw_input("Enter the name of the sample file:")
+    file_name += ".wav"
+    f = open(file_name,'wb')
+    f.write(p.read())
+    f.close()
+    print "Sample saved to ", file_name
+
+    # Play file
+    play_file(file_name,"mplayer")
+
+def play_file(file,player='mplayer'):
+    try:
+        os.system(player + " "+ file)
+    except:
+        print "Couldn't use %s to play file" % (player)
+
+if(__name__ == '__main__'):
+    sample()
